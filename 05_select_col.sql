060af7b8eb618ac3af2ff2876dbef6855e738c10
Kevin Richardson <Krsmjogos@outlook.com>
2025-08-28 21:00:40 -0300

    Novas colunas, ORDER BY, CASE WHEN

---
+++ 05_select_col.sql
@@ -0,0 +1,8 @@
+SELECT IdCliente,
+        QtdePontos,
+        QtdePontos + 10 AS QtdePontosPlus10,
+        QtdePontos * 2 AS QtdePontosDouble,
+        datetime(substr(DtCriacao,1,19)) AS DtCriacaoNova,
+        strftime('%w',datetime(substr(DtCriacao,1,19))) AS diaSemana
+
+FROM clientes
---
+++ 07_case.sql
@@ -0,0 +1,21 @@
+-- Intervalos
+-- De 0 a 500       -> Ponei
+-- De 501  a 1000   -> Ponei Premium
+-- De 1001  a 5000  -> Mago Aprendiz
+-- De 5001  a 10000 -> Mago Mestre
+-- De +10001        -> Mago Supremo
+
+SELECT  IdCliente,
+        QtdePontos,
+        CASE
+            WHEN QtdePontos <= 500 THEN 'Ponei'
+            WHEN QtdePontos > 500 AND QtdePontos <= 1000 THEN 'Ponei Premium'
+            WHEN QtdePontos <= 5000 THEN 'Mago Aprendiz'
+            WHEN QtdePontos <= 10000 THEN 'Mago Mestre'
+            ELSE 'Mago Supremo'
+        END AS NomeGrupo
+
+FROM clientes
+
+ORDER BY QtdePontos DESC
+
