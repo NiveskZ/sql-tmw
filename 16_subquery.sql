339b22dc7e1a8cced109a4752b57f83ad03baeeb
Kevin Richardson <Krsmjogos@outlook.com>
2025-09-02 14:18:01 -0300

    SubQueries e CTEs

---
+++ 16_subquery.sql
@@ -0,0 +1,9 @@
+SELECT *
+-- Demonstração de subquery no FROM
+FROM ( 
+    SELECT *
+    FROM transacoes 
+    WHERE DtCriacao >= '2025-01-01'
+)
+
+WHERE DtCriacao < '2025-07-01'
---
+++ 17_cte.sql
@@ -0,0 +1,39 @@
+-- CTE: COMMON TABLE EXPRESSION
+-- SELECT count(DISTINCT IdCliente)
+
+-- FROM transacoes AS t1
+
+-- WHERE t1.IdCliente IN (
+--     SELECT DISTINCT IdCliente
+--     FROM transacoes
+--     WHERE substr(DtCriacao,1,10) = '2025-08-25'
+-- )
+-- AND substr(t1.DtCriacao,1,10) = '2025-08-29'
+
+WITH tb_cliente_primeiro_dia AS (
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE substr(DtCriacao,1,10) = '2025-08-25'
+),
+
+tb_cliente_ultimo_dia AS (
+    SELECT DISTINCT IdCliente
+    FROM transacoes
+    WHERE substr(DtCriacao,1,10) = '2025-08-29'
+),
+
+tb_join AS (
+    SELECT t1.IdCliente AS primCliente,
+            t2.IdCliente AS ultCliente
+
+    FROM tb_cliente_primeiro_dia AS t1
+
+    LEFT JOIN tb_cliente_ultimo_dia AS t2
+    ON t1.IdCliente = t2.IdCliente
+)
+
+SELECT count(primCliente),
+        count(ultCliente),
+        1. * count(ultCliente)/ count(primCliente) AS proporcao
+
+FROM tb_join
